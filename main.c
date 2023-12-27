b12d70579529385ff36355785e15849dec0abdd8
AquilaFasciata <zachisaunders12@gmail.com>
2023-12-27 13:05:58 -0500

    Added some stuff and a character function

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+    ASCII Values
+    0-9 = 48-57
+    A-Z = 65-90
+    a-z = 97-122
+*/
+
+
+
+int isSymbol(char character) {
+    if (character < 48) {return 1;}
+        else if (character > 57 && character < 65) {return 1;}
+        else if (character > 90 && character < 97) {return 1;}
+        else if (character > 122) {return 1;}
+        else {return 0;}
+}
 
 int main() {
-    char address[9];
+    char address[18];
+    int addressBytes[18];
+    int i = 0;
+
     printf("What is the MAC address?\n");
     scanf("%17s", &address);
 
-    for (i = 0; i < strlen(address); i++) {
-        // Check for the two common separator and skip them. 
-        if (address[i] == '-' || address[i] == ':') { continue; }
+    for (i = 0; i < strlen(&address); i++) {
+        char pair[2];
+        if ( isSymbol(address[i+1]) ) {continue;}
 
-        addressBytes[i] = (int) strtol(&address[i], NULL, 16);
-        printf("%x %d\n", addressBytes[i], addressBytes[i]);
+           
     }
 
     return 0;

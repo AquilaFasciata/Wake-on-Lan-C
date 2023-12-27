ffd1c6910de693dba36b53a5d793222ec3887b2f
Zach Saunders <zachisaunders12@gmail.com>
2023-12-27 11:53:43 -0500

    Mores

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 int main() {
     char address[9];
     printf("What is the MAC address?\n");
-    scanf("%8s\n", &address);
+    scanf("%17s", &address);
 
-    printf("The string was %s.", address);
+    for (i = 0; i < strlen(address); i++) {
+        // Check for the two common separator and skip them. 
+        if (address[i] == '-' || address[i] == ':') { continue; }
+
+        addressBytes[i] = (int) strtol(&address[i], NULL, 16);
+        printf("%x %d\n", addressBytes[i], addressBytes[i]);
+    }
 
     return 0;
 }

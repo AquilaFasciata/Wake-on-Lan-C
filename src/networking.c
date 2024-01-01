ca53f2664cc1a070598bc7a7dba4b3f24f9a2d42
Zach <zachisaunders12@gmail.com>
2024-01-01 00:42:10 -0500

    Changing to work the way that I know how

--- src/networking.c
+++ src/networking.c
@@ -1,10 +1,14 @@
 #include <sys/socket.h>
 
-int * create_magic_packet(int* address[]) {
-    int* packet[102];
+int create_magic_packet(int *address[],  int *destination[]) {
     int i = 0;
-    
+
+    // TODO Implement Program Error Signals
+    if (sizeof(*destination) > 102) {
+        
+    }
+
     for (i = 0; i < 6; i++) {
-        (*packet)[i] = *(address + i);
+        *(destination + i) = *(address + i);
     }
 }
